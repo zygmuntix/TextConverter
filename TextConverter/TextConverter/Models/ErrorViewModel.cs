3466fb40a23ad3e11b7380ef87f2d15d63cf06e4
zygmuntix <zygmuntsze@gmail.com>
2020-09-30 16:07:44 +0200

    Initial project

---
+++ TextConverter/TextConverter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TextConverter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
